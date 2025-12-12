9328f92e6c6b355105d7f48dcbd6fd0c1e36ef12
MrTastyDuck <168537601+MrTastyDuck@users.noreply.github.com>
2025-12-12 09:20:51 +0100

    hinzufügen von update Formular und Aufrufsfunktion
    Fehlerhaft!!!!

--- public/index.php
+++ public/index.php
@@ -30,6 +30,27 @@ if (isset($_GET['add'])) {
     }
 }
 
+//UPDATEN
+if (isset($_GET['edit'])) {
+    $old_isbn = mysqli_real_escape_string($db, $_GET['old_isbn']);
+    $isbn = mysqli_real_escape_string($db, $_GET['edit_isbn']);
+    $titel = mysqli_real_escape_string($db, $_GET['edit_titel']);
+    $autor = mysqli_real_escape_string($db, $_GET['edit_autor']);
+    $beschreibung = mysqli_real_escape_string($db, $_GET['edit_beschreibung']);
+
+    if ($isbn !== "" && $titel !== "" && $autor !== "" && $beschreibung !== "") {
+        $sql_update = "
+            UPDATE book SET 
+                isbn='$isbn',
+                titel='$titel',
+                autor='$autor',
+                beschreibung='$beschreibung'
+            WHERE isbn='$old_isbn'
+        ";
+        mysqli_query($db, $sql_update);
+    }
+}
+
 // SUCHEN
 $sql = "SELECT isbn, titel, autor, beschreibung FROM book";
 $filter = [];
@@ -119,6 +140,7 @@ mysqli_close($db);
             <th>Titel</th>
             <th>Autor</th>
             <th>Beschreibung</th>
+            <th>Aktion</th>
         </tr>
 
         <?php foreach ($rows as $zeile): ?>
@@ -127,8 +149,41 @@ mysqli_close($db);
                 <td><?= htmlspecialchars($zeile["titel"]) ?></td>
                 <td><?= htmlspecialchars($zeile["autor"]) ?></td>
                 <td><?= htmlspecialchars($zeile["beschreibung"]) ?></td>
+                <td>
+                    <a href="?edit_form=1&isbn=<?= urlencode($zeile['isbn']) ?>">Bearbeiten</a>
+                </td>
             </tr>
         <?php endforeach; ?>
+    </table>
+
+    <?php
+    if (isset($_GET['edit_form'])) {
+        $edit_isbn = $_GET['isbn'];
+
+        $sql_edit = "SELECT * FROM book WHERE isbn='" . mysqli_real_escape_string($db, $edit_isbn) . "'";
+        $res_edit = mysqli_query($db, $sql_edit);
+        $buch = mysqli_fetch_assoc($res_edit);
+    ?>
+        <h1>Buch bearbeiten</h1>
+        <form method="get">
+            <input type="hidden" name="old_isbn" value="<?= htmlspecialchars($buch['isbn']) ?>">
+
+            <label>ISBN:</label><br>
+            <input type="text" name="edit_isbn" value="<?= htmlspecialchars($buch['isbn']) ?>"><br><br>
+
+            <label>Titel:</label><br>
+            <input type="text" name="edit_titel" value="<?= htmlspecialchars($buch['titel']) ?>"><br><br>
+
+            <label>Autor:</label><br>
+            <input type="text" name="edit_autor" value="<?= htmlspecialchars($buch['autor']) ?>"><br><br>
+
+            <label>Beschreibung:</label><br>
+            <textarea name="edit_beschreibung"><?= htmlspecialchars($buch['beschreibung']) ?></textarea><br><br>
+
+            <button type="submit" name="edit">Speichern</button>
+        </form>
+    <?php } ?>
+
     </table>
 
 </body>
